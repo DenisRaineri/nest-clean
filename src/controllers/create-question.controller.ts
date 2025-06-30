78c84e67a13934733f7746780e98d09ee1b88ae6
DenisRaineri <denispraineri@gmail.com>
2025-06-30 17:54:35 -0300

    feat: Add guards (jwt auth guard / jwt strategy )

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('/questions')
+@UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
   constructor() {}
 
