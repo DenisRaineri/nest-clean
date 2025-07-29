84e40c811411034ae6b4f9ec54d401fe5a42f251
DenisRaineri <denispraineri@gmail.com>
2025-07-29 15:37:46 -0300

    feat: teste bug

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { CreateAccountController } from './controllers/create-account.controller';
+
 import { AuthenticateController } from './controllers/authenticate.controller';
+import { CreateAccountController } from './controllers/create-account.controller';
 import { CreateQuestionController } from './controllers/create-question.controller';
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller';
 import { DatabaseModule } from '../database/database.module';
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question';
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions';
 
 @Module({
   imports: [DatabaseModule],
@@ -13,5 +16,6 @@ import { DatabaseModule } from '../database/database.module';
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
+  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
 })
 export class HttpModule {}
